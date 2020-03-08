fae3dc64d0f1083668029646e74482c67dde8e15
LolGetRektScrub <54378437+LolGetRektScrub@users.noreply.github.com>
2020-03-07 23:51:10 -0500

    Update Capital.js

--- Capital.js
+++ Capital.js
@@ -1,19 +1,12 @@
-const median = arr => {
-    const mid = Math.floor(arr.length / 2),
-    nums = Input.sort(function(a, b){return a - b});
-    return Input % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
-}
-function button(){
+  function button(){
     var inputArray = [];
   var size = 5; 
   
   for(var i=0; i<size; i++) {
     
     inputArray[i] = prompt('Enter Element ' + (i+1));
-    inputArray.sort(function(a, b){return a - b});
-    document.getElementById("demo").innerHTML = inputArray;
+    inputArray.sort();
+    document.getElementById("demo").innerHTML = inputArray.map(function(x){ return x.toUpperCase() });
   }
-  window.Input = inputArray;
-  var abra = median(Input);
-  console.log(abra);
-}
+  var str = String(inputArray).toUpperCase().split(",");
+  }
